158bfc55e1662fa5b09e0a644b6cad6bb741f2cf
KubaD4 <kdb4@ymail.com>
2025-05-17 18:49:31 +0200

    migliorato ma ancora ha un po' di problemi

--- CONVERTER/converter.py
+++ CONVERTER/converter.py
@@ -1,10 +1,17 @@
+import argparse
 from prolog2up import main
 
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Specify the Prolog file name.")
+parser.add_argument("filename", help="Name of the Prolog file")
+args = parser.parse_args()
+
 # Path al file Prolog
-prolog_file = "PROLOG/small_kb_hl.pl"
+prolog_file = "PROLOG/" + args.filename
 
 # Nome del file di output PDDL
 pddl_output = "converter_pddl_result"
 
 # Esegui la conversione
 problem = main(prolog_file, pddl_output)
+
